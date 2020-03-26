0917220b1c6551ee2c4fabf05c5716e0e1c1d174
唯刃 <weiren@dian.so>
2020-03-26 19:01:04 +0800

    feat: auto open generated html (fixes #10)
    it also fixes #7, by moving the function call to its constructor. :)

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-require("./lib/profiler")();
+const profile = require("./lib/profiler");
 let isListeningOnStdin = false;
+const defaultOpts = {
+  profilePath: void 0,
+  open: true // auto open the generated html
+};
 
 module.exports = class CpuProfileWebpackPlugin {
+  constructor(opts) {
+    profile(Object.assign({}, defaultOpts, opts));
+  }
   apply(compiler) {
     compiler.hooks.watchRun.tapAsync(
       "CpuProfileWebpackPlugin",
